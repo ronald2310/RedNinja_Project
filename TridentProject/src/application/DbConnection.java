b0a3ab88d24699efb4eba301f0c42bdcefa0db86
amonter4 <montero.andris@gmail.com>
2016-10-30 22:50:30 -0500

    Add files via upload

--- TridentProject/src/application/DbConnection.java
+++ TridentProject/src/application/DbConnection.java
@@ -5,8 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
+	
+	public static void main(String[] args)
+	{
+		
+		final String connectionURL = "jdbc:sqlserver://localhost:1433;databaseName=Demo-Database;user=user;password=Pass123456";
+		
+		try
+		{
+			Connection con = DriverManager.getConnection(connectionURL);
+			System.out.println("Connected");
+			con.close();
+			System.out.println("Connection Closed");
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Error: " + ex.getMessage());
+		}
+	}
 
-	public void main(String[] args)  throws  ClassNotFoundException, SQLException{
+	/*public void main(String[] args)  throws  ClassNotFoundException, SQLException{
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String connectionURL = "jdbc:sqlserver://localhost:1433;databaseName=Demo-Database;user=user;password=Pass123456";
         Connection con = DriverManager.getConnection(connectionURL);
@@ -14,4 +32,5 @@ public class DbConnection {
         System.out.println("You are connected ");
         System.out.println(con);
 	}
+	*/
 }
